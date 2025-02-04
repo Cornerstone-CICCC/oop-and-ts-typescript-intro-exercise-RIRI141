484d9992b2cc059cc33e8ba93a636c2c969bf549
Ryoga <isimaruru.1712@gmail.com>
2025-02-04 11:58:00 -0800

    Done

--- dist/ex02.js
+++ dist/ex02.js
@@ -1,14 +1,13 @@
+"use strict";
 // 2. Create a function named `isPalindrome` that takes a string as a parameter.
 //    The function should return `true` if the string is a palindrome (reads the same backward as forward),
 //    otherwise, return `false`.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function isPalindrome(word: string): boolean {
-   const capitalizedWord = word.toLowerCase();
-   const reverseWord = capitalizedWord.split("").reverse().join("");
-   return reverseWord === capitalizedWord;
+function isPalindrome(word) {
+    const capitalizedWord = word.toLowerCase();
+    const reverseWord = capitalizedWord.split("").reverse().join("");
+    return reverseWord === capitalizedWord;
 }
-
 // Expected output:
 console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("hello"));   // false
+console.log(isPalindrome("hello")); // false
